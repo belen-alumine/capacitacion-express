e35d123e98285d13d06be77a248807de0b596d3c
bcallegaris@bleett.com <bcallegaris@bleett.com>
2025-08-04 23:41:05 -0300

    feat: ejercicios middleware

--- myApp.js
+++ myApp.js
@@ -1,32 +1,35 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
-require('dotenv').config();
 
 //app.get('/', function(req, res) {
 //    res.sendFile(__dirname + '/views/index.html');
 //});
-//
+
 //app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/json', function(req, res) {
-    var response = 'Hello Json'
-    if (process.env.MESSAGE_STYLE === 'uppercase') {
-        response = response.toUpperCase();
-    } 
-    res.json({"message": response});
-
+//app.get('/json', (req, res) => {
+//    const style = process.env.MESSAGE_STYLE;
+//    const message = style && style.toLowerCase() === 'uppercase'
+//      ? "HELLO JSON"
+//      : "Hello json";
+//  
+//    res.json({ message });
+//  });
+  
+//app.use(function(req, res, next) {
+//    console.log(`${req.method} ${req.path} - ${req.ip}`);
+//    next(); // para que la app no se bloquee: investigar mejor esto
+//});
+  
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+  }, function(req, res) {
+    res.json({ time: req.time });
 });
-
-
-
-
-
-
-
-
-
-
-
+  
 
 
 
